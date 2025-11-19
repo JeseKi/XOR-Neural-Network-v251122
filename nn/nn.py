0e027f366a0b7fa6d6a9b3cc9bad3e9c22730892
Jese Ki <2094901072@qq.com>
2025-11-19 11:17:00 +0800

    fix: Correct bias application in forward pass of XORNeuralNetwork

--- nn/nn.py
+++ nn/nn.py
@@ -32,19 +32,19 @@ class XORNeuralNetwork:
         self.hidden_to_output_bias: np.ndarray = self.rng.uniform(size=(1,))
 
     def forward(self, input: Input) -> Output:
-        z1 = input.x1 * self.input_to_hidden_weights[0][0] + self.input_to_hiden_bias[0]
-        z2 = input.x2 * self.input_to_hidden_weights[0][1] + self.input_to_hiden_bias[0]
-        z3 = input.x1 * self.input_to_hidden_weights[1][0] + self.input_to_hiden_bias[1]
-        z4 = input.x2 * self.input_to_hidden_weights[1][1] + self.input_to_hiden_bias[1]
+        z1 = input.x1 * self.input_to_hidden_weights[0][0]
+        z2 = input.x2 * self.input_to_hidden_weights[0][1]
+        z3 = input.x1 * self.input_to_hidden_weights[1][0]
+        z4 = input.x2 * self.input_to_hidden_weights[1][1]
 
-        h1 = self.sigmoid(z1 + z2)
-        h2 = self.sigmoid(z3 + z4)
+        h1 = self.sigmoid(z1 + z2 + self.input_to_hiden_bias[0])
+        h2 = self.sigmoid(z3 + z4 + self.input_to_hiden_bias[1])
         self.input_to_hidden_outputs = np.array([h1, h2])
 
-        z5 = h1 * self.hidden_to_output_weights[0] + self.hidden_to_output_bias[0]
-        z6 = h2 * self.hidden_to_output_weights[1] + self.hidden_to_output_bias[0]
+        z5 = h1 * self.hidden_to_output_weights[0]
+        z6 = h2 * self.hidden_to_output_weights[1]
 
-        h3 = self.sigmoid(z5 + z6)
+        h3 = self.sigmoid(z5 + z6 + self.hidden_to_output_bias[0])
         self.hidden_to_output_outputs = np.array([h3])
 
         return Output(y=h3)

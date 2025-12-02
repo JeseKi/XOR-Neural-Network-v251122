f33dea75259fb0837860aa5e866fef020e63e2aa
JeseKi <2094901072@qq.com>
2025-12-02 11:43:51 +0800

    refactor: 将 delta 全部改为 grad

--- nn/nn.py
+++ nn/nn.py
@@ -48,24 +48,24 @@ class XORNeuralNetwork:
         input_hidden_arr = np.array([[input.x1, input.x2]]) # shape(1, 2)
         loss = self.mean_squared_error(self.output_outputs[0], answer.y)
 
-        delta_loss = self.mean_squared_error_derivative(
+        grad_loss = self.mean_squared_error_derivative(
             self.output_outputs[0], answer.y
         )
-        delta_output_outputs = self.sigmoid_derivative(
+        grad_output_outputs = self.sigmoid_derivative(
             self.output_outputs[0]
         )
-        delta_output = delta_loss * delta_output_outputs # shape(1, 1)
-        delta_output_weights = delta_output * self.hidden_outputs # shape(1, 2)
-        delta_b3 = delta_output # shape(1, 1)
+        grad_output = grad_loss * grad_output_outputs # shape(1, 1)
+        grad_output_weights = grad_output * self.hidden_outputs # shape(1, 2)
+        grad_output_bias = grad_output # shape(1, 1)
 
-        delta_hidden_outputs = self.sigmoid_derivative(self.hidden_outputs) # shape(1, 2)
-        delta_hidden = (delta_output @ self.output_weights.T) * delta_hidden_outputs # shape(1, 2)
-        delta_hidden_weights = input_hidden_arr.T @ delta_hidden # shape(2, 2)
-        delta_hidden_bias = delta_hidden # shape(1, 2)
+        grad_hidden_outputs = self.sigmoid_derivative(self.hidden_outputs) # shape(1, 2)
+        grad_hidden = (grad_output @ self.output_weights.T) * grad_hidden_outputs # shape(1, 2)
+        grad_hidden_weights = input_hidden_arr.T @ grad_hidden # shape(2, 2)
+        grad_hidden_bias = grad_hidden # shape(1, 2)
 
-        self.hidden_weights -= self.learning_rate * delta_hidden_weights
-        self.hidden_bias -= self.learning_rate * delta_hidden_bias
-        self.output_weights -= self.learning_rate * delta_output_weights.T
-        self.output_bias -= self.learning_rate * delta_b3
+        self.hidden_weights -= self.learning_rate * grad_hidden_weights
+        self.hidden_bias -= self.learning_rate * grad_hidden_bias
+        self.output_weights -= self.learning_rate * grad_output_weights.T
+        self.output_bias -= self.learning_rate * grad_output_bias
 
         return loss

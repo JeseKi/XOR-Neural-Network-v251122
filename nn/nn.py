1c8ce568f1040965bbbe98c6eb422cb786f88fd6
JeseKi <2094901072@qq.com>
2025-12-02 11:52:33 +0800

    refactor: 进一步重构命名

--- nn/nn.py
+++ nn/nn.py
@@ -24,48 +24,48 @@ class XORNeuralNetwork:
 
         # init variable
         self.W1: np.ndarray = self.rng.uniform(size=(2, 2), low=-1, high=1)
-        self.W1_pred: np.ndarray = np.zeros(shape=(1,2))
-        self.W1_bias: np.ndarray = self.rng.uniform(size=(1,2), low=-1, high=1)
+        self.W1_outputs: np.ndarray = np.zeros(shape=(1,2))
+        self.bias1: np.ndarray = self.rng.uniform(size=(1,2), low=-1, high=1)
 
         self.W2: np.ndarray = self.rng.uniform(size=(2, 1), low=-1, high=1)
-        self.W2_pred: np.ndarray = np.zeros(shape=(1,1))
-        self.W2_bias: np.ndarray = self.rng.uniform(size=(1,1), low=-1, high=1)
+        self.W2_outputs: np.ndarray = np.zeros(shape=(1,1))
+        self.bias2: np.ndarray = self.rng.uniform(size=(1,1), low=-1, high=1)
 
     def forward(self, input: Input) -> Target:
         input_arr = np.array([[input.x1, input.x2]]) # shape(1, 2)
 
-        z1 = input_arr @ self.W1 + self.W1_bias # shape(1, 2)
+        z1 = input_arr @ self.W1 + self.bias1 # shape(1, 2)
         h1 = self.sigmoid(z1)
-        self.W1_pred = h1
+        self.W1_outputs = h1
 
-        z2 = h1 @ self.W2 + self.W2_bias # shape(1, 1)
+        z2 = h1 @ self.W2 + self.bias2 # shape(1, 1)
         h2 = self.sigmoid(z2)
-        self.W2_pred = h2
+        self.W2_outputs = h2
 
         return Target(y=h2[0])
 
     def backward(self, input: Input, target: Target) -> float:
         input_hidden_arr = np.array([[input.x1, input.x2]]) # shape(1, 2)
-        loss = self.mean_squared_error(self.W2_pred[0], target.y)
+        loss = self.mean_squared_error(self.W2_outputs[0], target.y)
 
         grad_loss = self.mean_squared_error_derivative(
-            self.W2_pred[0], target.y
+            self.W2_outputs[0], target.y
         )
         grad_output_outputs = self.sigmoid_derivative(
-            self.W2_pred[0]
+            self.W2_outputs[0]
         )
         grad_output = grad_loss * grad_output_outputs # shape(1, 1)
-        grad_output_weights = grad_output * self.W1_pred # shape(1, 2)
+        grad_output_weights = grad_output * self.W1_outputs # shape(1, 2)
         grad_output_bias = grad_output # shape(1, 1)
 
-        grad_hidden_outputs = self.sigmoid_derivative(self.W1_pred) # shape(1, 2)
+        grad_hidden_outputs = self.sigmoid_derivative(self.W1_outputs) # shape(1, 2)
         grad_hidden = (grad_output @ self.W2.T) * grad_hidden_outputs # shape(1, 2)
         grad_hidden_weights = input_hidden_arr.T @ grad_hidden # shape(2, 2)
         grad_hidden_bias = grad_hidden # shape(1, 2)
 
         self.W1 -= self.learning_rate * grad_hidden_weights
-        self.W1_bias -= self.learning_rate * grad_hidden_bias
+        self.bias1 -= self.learning_rate * grad_hidden_bias
         self.W2 -= self.learning_rate * grad_output_weights.T
-        self.W2_bias -= self.learning_rate * grad_output_bias
+        self.bias2 -= self.learning_rate * grad_output_bias
 
         return loss
--- nn/train.py
+++ nn/train.py
@@ -24,8 +24,8 @@ def train(
                     loss=total_loss / len(INPUT),
                     input_to_hidden_weights=neural_network.W1.copy(),
                     hidden_to_output_weights=neural_network.W2.copy(),
-                    input_to_hidden_bias=neural_network.W1_bias.copy(),
-                    hidden_to_output_bias=neural_network.W2_bias.copy(),
+                    input_to_hidden_bias=neural_network.bias1.copy(),
+                    hidden_to_output_bias=neural_network.bias2.copy(),
                 )
             )
     return history

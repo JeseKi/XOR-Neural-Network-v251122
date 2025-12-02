262a173469b9ff00cc0185c241b68993d78af3d7
JeseKi <2094901072@qq.com>
2025-12-02 11:32:47 +0800

    refactor: 将反向传播中的 gradient 全部改为 delta

--- nn/nn.py
+++ nn/nn.py
@@ -48,24 +48,24 @@ class XORNeuralNetwork:
         input_hidden_arr = np.array([[input.x1, input.x2]]) # shape(1, 2)
         loss = self.mean_squared_error(self.output_outputs[0], answer.y)
 
-        gradient_loss = self.mean_squared_error_derivative(
+        delta_loss = self.mean_squared_error_derivative(
             self.output_outputs[0], answer.y
         )
-        gradient_output_local = self.sigmoid_derivative(
+        delta_output_outputs = self.sigmoid_derivative(
             self.output_outputs[0]
         )
-        gradient_output = gradient_loss * gradient_output_local # shape(1, 1)
-        gradient_output_weights = gradient_output * self.hidden_outputs # shape(1, 2)
-        gradient_b3 = gradient_output # shape(1, 1)
+        delta_output = delta_loss * delta_output_outputs # shape(1, 1)
+        delta_output_weights = delta_output * self.hidden_outputs # shape(1, 2)
+        delta_b3 = delta_output # shape(1, 1)
 
-        gradient_hidden_local = self.sigmoid_derivative(self.hidden_outputs) # shape(1, 2)
-        gradient_hidden = (gradient_output @ self.output_weights.T) * gradient_hidden_local # shape(1, 2)
-        gradient_hidden_weights = input_hidden_arr.T @ gradient_hidden # shape(2, 2)
-        gradient_hidden_bias = gradient_hidden # shape(1, 2)
+        delta_hidden_outputs = self.sigmoid_derivative(self.hidden_outputs) # shape(1, 2)
+        delta_hidden = (delta_output @ self.output_weights.T) * delta_hidden_outputs # shape(1, 2)
+        delta_hidden_weights = input_hidden_arr.T @ delta_hidden # shape(2, 2)
+        delta_hidden_bias = delta_hidden # shape(1, 2)
 
-        self.hidden_weights -= self.learning_rate * gradient_hidden_weights
-        self.hidden_bias -= self.learning_rate * gradient_hidden_bias
-        self.output_weights -= self.learning_rate * gradient_output_weights.T
-        self.output_bias -= self.learning_rate * gradient_b3
+        self.hidden_weights -= self.learning_rate * delta_hidden_weights
+        self.hidden_bias -= self.learning_rate * delta_hidden_bias
+        self.output_weights -= self.learning_rate * delta_output_weights.T
+        self.output_bias -= self.learning_rate * delta_b3
 
         return loss
